04d928b39cf9a8478633f3843a667677f13ea198
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-14 09:19:54 +0100

    Squashed 'apps/web-worker/' content from commit 32e5911
    git-subtree-dir: apps/web-worker
    git-subtree-split: 32e5911be6c5de95d6c70b8cac264c3d2baad8ed

---
+++ run.sh
@@ -0,0 +1,3 @@
+git subtree add --prefix=packages/svelte-leaflet https://github.com/valiantlynx/svelte-leaflet.git valiantlynx-turborepo --squash
+git subtree pull --prefix=packages/svelte-leaflet https://github.com/valiantlynx/svelte-leaflet.git valiantlynx-turborepo --squash
+git subtree push --prefix=packages/svelte-leaflet https://github.com/valiantlynx/svelte-leaflet.git valiantlynx-turborepo
