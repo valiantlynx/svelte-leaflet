a6246c691b61081b053640c94ec7613afc292b8c
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-28 06:10:49 +0100

    Refactor tile-layer component import and update page title

--- src/routes/components/tile-layer/+page.svelte
+++ src/routes/components/tile-layer/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Polygon from '$lib/site-components/Polygon.svelte';
+	import TileLayer from '$lib/site-components/TileLayer.svelte';
 	import { ValiantRichText } from '@valiantlynx/svelte-rich-text';
 	import { initialData } from './data';
 	import {Breadcrumbs} from '@valiantlynx/svelte-ui';
@@ -14,18 +14,18 @@
 			url: '/components'
 		},
 		{
-			name: 'polygon',
-			url: '/components/polygon'
+			name: 'tile-layer',
+			url: '/components/tile-layer'
 		}
 	];
 </script>
 
 <Breadcrumbs {crumbs} />
 
-<h1 class="text-4xl font-bold text-center">Polygon</h1>
+<h1 class="text-4xl font-bold text-center">Tile Layer</h1>
 
 <div class="m-10">
-	<Polygon />
+	<TileLayer />
 </div>
 
 <ValiantRichText viewMode={true} {initialData} />
