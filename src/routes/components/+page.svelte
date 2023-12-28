977449bb36f7553cb038a4d441bc59681f4a32c2
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-28 08:10:36 +0100

    Add components and FAQs sections to layout.svelte

--- src/routes/components/+page.svelte
+++ src/routes/components/+page.svelte
@@ -1,27 +1,40 @@
 <script>
-
+    // Sample data for components
+    let components = [
+        { name: 'Router', link: '/components/router', description: 'Efficient routing management.', icon: '/icons/router.svg' },
+        { name: 'Leaflet Map', link: '/components/leaflet-map', description: 'Interactive map integration.', icon: '/icons/map.svg' },
+        { name: 'Polygon', link: '/components/polygon', description: 'Draw and edit polygons.', icon: '/icons/polygon.svg' },
+        { name: 'Marker', link: '/components/marker', description: 'Place markers on the map.', icon: '/icons/marker.svg' },
+		{ name: 'Marker Icon', link: '/components/marker-icon', description: 'Customize your markers.', icon: '/icons/marker-icon.svg' },
+		{ name: 'Polyline', link: '/components/polyline', description: 'Draw and edit polylines.', icon: '/icons/polyline.svg' },
+		{ name: 'Popup', link: '/components/popup', description: 'Add popups to your map.', icon: '/icons/popup.svg' },
+		{ name: 'Tooltip', link: '/components/tooltip', description: 'Add tooltips to your map.', icon: '/icons/tooltip.svg' },
+		{ name: 'Tile Layer', link: '/components/tile-layer', description: 'Add tile layers to your map.', icon: '/icons/tile-layer.svg' },
+		{ name: 'Tile Layer WMS', link: '/components/tile-layer-wms', description: 'Add WMS tile layers to your map.', icon: '/icons/tile-layer-wms.svg' },
+		{ name: 'Rectangle', link: '/components/rectangle', description: 'Draw and edit rectangles.', icon: '/icons/rectangle.svg' },
+		{ name: 'Circle', link: '/components/circle', description: 'Draw and edit circles.', icon: '/icons/circle.svg' },
+		{ name: 'GeoJSON', link: '/components/geojson', description: 'Add GeoJSON to your map.', icon: '/icons/geojson.svg' },
+		{ name: 'Scale Control', link: '/components/scale-control', description: 'Add a scale control to your map.', icon: '/icons/scale-control.svg' },
+		{ name: 'Circle Marker', link: '/components/circle-marker', description: 'Draw and edit circle markers.', icon: '/icons/circle-marker.svg' },
+		{ name: 'Div Icon', link: '/components/div-icon', description: 'Add div icons to your map.', icon: '/icons/div-icon.svg' },
+		{ name: 'Image Overlay', link: '/components/image-overlay', description: 'Add image overlays to your map.', icon: '/icons/image-overlay.svg' }
+    ];
 </script>
 
 <div class="container mx-auto p-8">
-	<h1 class="text-4xl font-bold text-center text-gray-800 mb-6">Components</h1>
-	<!-- Links to specific pages about each of the components -->
-	<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-center">
-		<a href="/components/router" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Router</a>
-		<a href="/components/leaflet-map" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Leaflet Map</a>
-		<a href="/components/polygon" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Polygon</a>
-		<a href="/components/marker" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Marker</a>
-		<a href="/components/marker-icon" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Marker Icon</a>
-		<a href="/components/polyline" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Polyline</a>
-		<a href="/components/popup" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Popup</a>
-		<a href="/components/tooltip" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Tooltip</a>
-		<a href="/components/tile-layer" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Tile Layer</a>
-		<a href="/components/tile-layer-wms" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Tile Layer WMS</a>
-		<a href="/components/rectangle" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Rectangle</a>
-		<a href="/components/circle" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Circle</a>
-		<a href="/components/geojson" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">GeoJSON</a>
-		<a href="/components/scale-control" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Scale Control</a>
-		<a href="/components/circle-marker" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Circle Marker</a>
-		<a href="/components/div-icon" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Div Icon</a>
-		<a href="/components/image-overlay" class="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">Image Overlay</a>
-	</div>
+    <h1 class="text-5xl font-bold text-center mb-10">Explore Our Components</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {#each components as component}
+            <div class="card card-bordered card-compact lg:card-normal bg-base-100 shadow-xl">
+                <figure><img src='/logo.jpg' alt="{component.name}" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">{component.name}</h2>
+                    <p>{component.description}</p>
+                    <div class="card-actions justify-end">
+                        <a href={component.link} class="btn btn-primary">Learn More / code</a>
+                    </div>
+                </div>
+            </div>
+        {/each}
+    </div>
 </div>
