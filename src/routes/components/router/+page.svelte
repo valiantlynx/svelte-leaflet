af6f6e51b9e5d155982e2914d83847379f0ce202
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-28 20:07:30 +0100

    Refactor instructions display in router page

--- src/routes/components/router/+page.svelte
+++ src/routes/components/router/+page.svelte
@@ -1,29 +1,9 @@
 <script>
 	import Route from '$lib/site-components/Route.svelte';
-	import { ValiantRichText } from '@valiantlynx/svelte-rich-text';
+	import { ValiantRichText, getData } from '@valiantlynx/svelte-rich-text';
 	import { initialData } from './data';
 	import { Breadcrumbs } from '@valiantlynx/svelte-ui';
 	import { routeCoordinates, instructions, summary } from '$lib/components/stores';
-
-	let instructionsHTML = '';
-
-	$: {
-		if ($instructions) {
-			instructionsHTML =
-				'<ul  class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">';
-			for (const instruction of $instructions) {
-				instructionsHTML += `
-		  <li class="w-auto text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white m-2">
-			<button type="button" class="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b border-gray-200 rounded-t-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-			  <span class="mdi mdi-sign-direction text-2xl"></span>
-			  ${instruction.text} ${instruction.distance / 1000} km
-			</button>
-		  </li>`;
-			}
-
-			instructionsHTML += '</ul>';
-		}
-	}
 
 	const crumbs = [
 		{
@@ -46,11 +26,23 @@
 <h1 class="text-4xl font-bold text-center">Router</h1>
 
 <div class="m-10 h-96">
-	<!-- <ImageOverlay	/> -->
 	<Route />
 </div>
 
 <h1 class="text-4xl font-bold text-center">instructions</h1>
-{@html instructionsHTML}
+<div class="m-10">
+	{#each $instructions as instruction}
+		<div class="flex flex-row justify-between">
+			<div class="flex flex-col">
+				<p>{instruction.text}</p>
+				<p>{instruction.distance / 1000} km</p>
+			</div>
+			<div class="flex flex-col">
+				<p>{instruction.duration / 60} min</p>
+				<p>{instruction.type}</p>
+			</div>
+		</div>
+	{/each}
+</div>
 
 <ValiantRichText {initialData} />
